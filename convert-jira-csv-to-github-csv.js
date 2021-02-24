0961105367f626cad28eaa933eb3365d3aca6f8f
Emma Bukacek <emma.bukacek@shopify.com>
2021-02-24 16:04:57 -0500

    Fix labels for the children of epics

--- convert-jira-csv-to-github-csv.js
+++ convert-jira-csv-to-github-csv.js
@@ -30,10 +30,12 @@ program
         const newColumns = ['title', 'body', 'labels', 'comments'];
 
         const descriptionIndex = cols.indexOf("Description"); // Body
-        const epicIndex = cols.indexOf("Custom field (Epic Name)"); // Label (Epic:<Name>)
+        const parentEpicNameIndex = cols.indexOf("Custom field (Epic Name)"); // Label (Epic:<Name>) for parent
+        const epicIndex = cols.indexOf("Custom field (Epic Link)"); // Label (Epic:<Name>) for child
         const issueTypeIndex = cols.indexOf("Issue Type"); // Label
         const summaryIndex = cols.indexOf("Summary"); // Title
         const fixVersion = cols.indexOf("Fix versions"); // Label
+        const issueKey = cols.indexOf("Issue key"); // For Epic Name -> Epic Link
 
         const commentsIndexes = cols.reduce((acc, name, idx) => {
           if (name !== "Comment") return acc;
@@ -45,6 +47,11 @@ program
           return [...acc, idx]
         }, []); // Label
 
+        const epicLinkToNameMap = rows.reduce((acc, row) => {
+          if (!row[parentEpicNameIndex]) return acc;
+          return {...acc, [row[issueKey]]: row[parentEpicNameIndex]};
+        }, {});
+
         const parsedRows = rows.map((row, idx) => {
           const newRow = [];
           const labels = [];
@@ -54,7 +61,14 @@ program
 
           // Labels
           labels.push(options.team);
-          if (row[epicIndex]) labels.push(`Epic:${row[epicIndex]}`);
+          if (row[epicIndex]) {
+            labels.push(`Epic:${epicLinkToNameMap[row[epicIndex]]}`);
+          }
+
+          if(row[parentEpicNameIndex]) {
+            labels.push(`Epic:${row[parentEpicNameIndex]}`);
+          }
+
           if (row[issueTypeIndex] === "Epic" || row[issueTypeIndex] === "Bug") {
             labels.push(row[issueTypeIndex]);
           }
